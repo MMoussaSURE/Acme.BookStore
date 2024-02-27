cc8bb6aa73882b2995eebf058220c7ff095b4659
mmoussa <mmoussa@MMOUSSA>
2024-02-27 10:19:09 +0200

    solve dll errors

--- src/Acme.BookStore.HttpApi.Host/Helpers/MyExceptionSubscriber.cs
+++ src/Acme.BookStore.HttpApi.Host/Helpers/MyExceptionSubscriber.cs
@@ -1,4 +1,5 @@
-﻿using System.Buffers.Text;
+﻿using Microsoft.Extensions.Logging;
+using System.Buffers.Text;
 using System.Threading.Tasks;
 using Volo.Abp.ExceptionHandling;
 
@@ -6,10 +7,16 @@ namespace Acme.BookStore.Helpers
 {
     public class MyExceptionSubscriber : ExceptionSubscriber
     {
+        private readonly ILogger<MyExceptionSubscriber> _logger;
+        public MyExceptionSubscriber(ILogger<MyExceptionSubscriber> logger)
+        {
+                _logger = logger;
+        }
         public async override Task HandleAsync(ExceptionNotificationContext context)
         {
             var exception = context.Exception;
             var exceptionMessage = context.Exception.Message;
+            _logger.LogException(exception,LogLevel.Error);
 
             // You can also perform any necessary actions based on the exception
             // Example: Sending an email notification to the administrator
